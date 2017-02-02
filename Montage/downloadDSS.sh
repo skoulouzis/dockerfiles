2b3f0fc5f44cf9424ffa810a34b87b4fe41f8a7c
skoulouzis <S.Koulouzis@uva.nl>
2017-02-02 14:11:35 +0100

    Aded env variable

---
+++ Montage/downloadDSS.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Pleiades Image creation BASH script.
+# Inseok Song, 2007
+
+MONTAGE_HOME=/root/Montage
+MONTAGE_BIN=$MONTAGE_HOME/bin
+
+for bands in DSS2B DSS2R DSS2IR; do echo Processing ${bands};
+mkdir $bands;
+cd $bands;
+mkdir raw projected;
+cd raw;
+$MONTAGE_BIN/mArchiveList dss ${bands} "56.5 23.75" 3 3 remote.tbl;
+done
