3fa6e205506e075d07bb07623eb117345a45f029
skoulouzis <S.Koulouzis@uva.nl>
2017-05-17 16:17:36 +0200

    check for empty values

--- ArgoDiffusion/scripts/generation_argo_big_data.py
+++ ArgoDiffusion/scripts/generation_argo_big_data.py
@@ -85,7 +85,7 @@ class Argo:
             for row in reader:
                 rownum += 1
                 #first line, header   
-                if rownum == 1:
+                if (rownum == 1) of (not row [3]):
                     #nothing to do
                     continue
                 # test position
