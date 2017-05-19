e5df51047ebe19b8fd3ad673b43023420a92a55a
skoulouzis <S.Koulouzis@uva.nl>
2017-05-19 17:42:22 +0200

    added conf to result

--- ArgoDiffusion/scripts/runParameterSweep.sh
+++ ArgoDiffusion/scripts/runParameterSweep.sh
@@ -50,7 +50,7 @@ function parseResult() {
     output_file=`jq -r .output_file configuration_new.json`
     output_file_size=$(wc -c <"$output_file")
     
-    conf=`jq configuration_new.json`
+    conf=`jq . configuration_new.json`
     echo "{" \"area\": $area, \"time_coverage\": $time_coverage, \"num_of_params\": $num_of_params, \"dataset_size\": $dataset_size, \"output_file_size\": $output_file_size, \"execution_time\": $execution_time,\"execution_date\": \"$date\" , \"configuration\": $conf "}"
 }
 
