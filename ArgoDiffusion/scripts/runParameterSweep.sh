c587af35f61d777e3534a68a48d2fac4fc7a75de
skoulouzis <S.Koulouzis@uva.nl>
2017-05-19 18:19:32 +0200

    added more runs

--- ArgoDiffusion/scripts/runParameterSweep.sh
+++ ArgoDiffusion/scripts/runParameterSweep.sh
@@ -39,7 +39,7 @@ function parseResult() {
     output_file_size=$(wc -c <"$output_file")
     
     conf=`jq . configuration_new.json`
-#     echo "{" \"area\": $area, \"time_coverage\": $time_coverage, \"num_of_params\": $num_of_params, \"dataset_size\": $dataset_size, \"output_file_size\": $output_file_size, \"execution_time\": $execution_time,\"execution_date\": \"$date\" , \"configuration\": $conf "}"
+    echo "{" \"area\": $area, \"time_coverage\": $time_coverage, \"num_of_params\": $num_of_params, \"dataset_size\": $dataset_size, \"output_file_size\": $output_file_size, \"execution_time\": $execution_time,\"execution_date\": \"$date\" , \"configuration\": $conf "}"
 }
 
 
@@ -50,10 +50,16 @@ function run() {
         # Set longitude
         for (( j=$MIN_LON; j<=$MAX_LON; j=j+$STEP ))
         do
-            for (( k=1; k<=25; k=k+5))
+            for (( k=1; k<=21; k=k+10))
             do
             count=0
+            date_count=0
             NEXT_DATE=$(date +"%Y-%m-%dT%H:%M:%SZ" -d "$DATE + $k year")
+            NEXT_DATE_SECONDS=`date -d "$NEXT_DATE" +%s`
+            if [ "$NEXT_DATE_SECONDS" -gt "$MAX_DATE_SECONDS" ]; then
+                NEXT_DATE=$MAX_DATE        
+            fi
+
             parameters=9
             while read l; do
                 count=$((count+1))
@@ -61,9 +67,8 @@ function run() {
                 if [ "$count" -gt "200" ]; then
                     count=0
                     newConf $i $j $NEXT_DATE $parameters
-                    echo $i $j $NEXT_DATE
-#                     ./generation_argo_big_data.csh configuration_new.json &> out
-#                     parseResult
+                    ./generation_argo_big_data.csh configuration_new.json &> out
+                    parseResult
                 fi
                 
             done <physical_parameter_keys.txt
@@ -77,10 +82,11 @@ MIN_LAT=-3
 MAX_LAT=35
 MIN_LON=31
 MAX_LON=44
-STEP=8
+STEP=20
 # Set date
 DATE=1999-01-01T00:00:19Z
-
+MAX_DATE=2017-04-13T00:07:18Z
+MAX_DATE_SECONDS=`date -d "$MAX_DATE" +%s`
 run
 
 #Atlantic
@@ -92,10 +98,10 @@ run
 
 
 
-#Atlantic
-MIN_LAT=-72
-MAX_LAT=142
-MIN_LON=-68
-MAX_LON=52
-run 
+# #Pacific
+# MIN_LAT=-72
+# MAX_LAT=142
+# MIN_LON=-68
+# MAX_LON=52
+# run 
 
