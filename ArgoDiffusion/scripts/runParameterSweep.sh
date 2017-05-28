b5a4c69e11f0740b02daf0a74d9f193c3699ec99
skoulouzis <S.Koulouzis@uva.nl>
2017-05-28 14:31:25 +0200

    change excution time cacluation

--- ArgoDiffusion/scripts/runParameterSweep.sh
+++ ArgoDiffusion/scripts/runParameterSweep.sh
@@ -45,7 +45,7 @@ function parse_ssh_result() {
     
     conf=`jq . $1`
     num_of_nodes=`wc -l $SSH_FILE | awk '{print $1}'`
-    echo "{" \"area\": $area, \"time_coverage\": $time_coverage, \"num_of_params\": $num_of_params, \"dataset_size\": $dataset_size, \"output_file_size\": $output_file_size, \"execution_time\": $execution_time,\"execution_date\": \"$EXECUTION_DATE\" , \"configuration\": $conf, \"num_of_nodes\": "}"
+    echo "{" \"area\": $area, \"time_coverage\": $time_coverage, \"num_of_params\": $num_of_params, \"dataset_size\": $dataset_size, \"output_file_size\": $output_file_size, \"execution_time\": $execution_time,\"execution_date\": \"$EXECUTION_DATE\" , \"configuration\": $conf, \"num_of_nodes\":$num_of_nodes "}"
 }
 
 
