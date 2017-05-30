b3934624bda72f41c58a65fc4a79eabc11fa8499
skoulouzis <S.Koulouzis@uva.nl>
2017-05-30 20:40:48 +0200

    dbg

--- ArgoDiffusion/scripts/runParameterSweep.sh
+++ ArgoDiffusion/scripts/runParameterSweep.sh
@@ -191,8 +191,9 @@ function run_ssh() {
         scp -i $KEY_PATH $ssh_count"_"configuration_new.json $node:/mnt/data/source &> /dev/null   
         node_ip=`echo $line | awk -F "@" '{print $2}'`
         touch /tmp/$node_ip.run
-        ssh $node -i $KEY_PATH "screen -L -dmS argoBenchmark bash ~/workspace/dockerfiles/ArgoDiffusion/scripts/runParameterSweep.sh -op=run -json_conf_file=/mnt/data/source/$ssh_count"_"configuration_new.json -maser_ip=$MY_IP" < /dev/null
+        echo ssh $node -i $KEY_PATH "screen -L -dmS argoBenchmark bash ~/workspace/dockerfiles/ArgoDiffusion/scripts/runParameterSweep.sh -op=run -json_conf_file=/mnt/data/source/$ssh_count"_"configuration_new.json -maser_ip=$MY_IP" < /dev/null
         ssh_count=$((ssh_count+1))
+        exit 
     done < $SSH_FILE
     block
     parse_ssh_result configuration_new.json
