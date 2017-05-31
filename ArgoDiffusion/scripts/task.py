47372950d80f44c0aaf8f12db756e0c321d5dac2
skoulouzis <S.Koulouzis@uva.nl>
2017-06-01 01:42:26 +0200

    don't block

--- ArgoDiffusion/scripts/task.py
+++ ArgoDiffusion/scripts/task.py
@@ -9,22 +9,27 @@ import json
 rabbit_host = sys.argv[1]
 rabbit_port = sys.argv[2]
 conf_file = sys.argv[3]
+op = sys.argv[4]
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbit_host, port=int(rabbit_port)))
 channel = connection.channel()
-channel.queue_declare(queue='task_queue', durable=True)
+queue = channel.queue_declare(queue='task_queue', durable=True)
 
-with open(conf_file) as json_data:
-    conf = json.load(json_data)
-message  = json.dumps(conf)
+if op is 'task':
+    with open(conf_file) as json_data:
+        conf = json.load(json_data)
+    message  = json.dumps(conf)
+    channel.basic_publish(exchange='',
+                        routing_key='task_queue',
+                        body=message,
+                        properties=pika.BasicProperties(
+                            delivery_mode = 2, 
+                        ))
+else:
+    print(queue.method.message_count)
 
-channel.basic_publish(exchange='',
-                      routing_key='task_queue',
-                      body=message,
-                      properties=pika.BasicProperties(
-                         delivery_mode = 2, # make message persistent
-                      ))
-#print(" [x] Sent %r" % message)
+
+
 connection.close()
 
 
