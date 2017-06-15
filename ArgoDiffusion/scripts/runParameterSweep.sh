c8bfca3a52170f7321818b3c104232d175c606ad
skoulouzis <S.Koulouzis@uva.nl>
2017-06-15 19:53:01 +0200

    add all params

--- ArgoDiffusion/scripts/runParameterSweep.sh
+++ ArgoDiffusion/scripts/runParameterSweep.sh
@@ -48,12 +48,10 @@ function parse_dist_result() {
     local output_file_size=$(wc -c <"$output_file")
      
     local conf=$(jq . $1)
-        
-    local num_of_nodes=`python getNumberOfConsumers.py $RMQ_HOST 15672 task_queue`
     
 #     dbg ${FUNCNAME[0]} "output_file_size: "$output_file_size
 #     if [ "$output_file_size" -gt "86" ]; then
-        echo "{" \"area\": $area, \"time_coverage\": $time_coverage, \"num_of_params\": $num_of_params, \"dataset_size\": $dataset_size, \"output_file_size\": $output_file_size, \"execution_time\": $execution_time,\"execution_date\": \"$EXECUTION_DATE\" , \"configuration\": $conf, \"num_of_nodes\":$num_of_nodes , \"executing_node\":\"$MY_IP\""}"
+        echo "{" \"area\": $area, \"time_coverage\": $time_coverage, \"num_of_params\": $num_of_params, \"dataset_size\": $dataset_size, \"output_file_size\": $output_file_size, \"execution_time\": $execution_time,\"execution_date\": \"$EXECUTION_DATE\" , \"configuration\": $conf, \"num_of_nodes\":$NUMBER_OF_NODES , \"executing_node\":\"$MY_IP\""}"
 #     fi
 }
 
@@ -221,27 +219,49 @@ function send_messages() {
     for new_file in $( ls *_configuration_new.json); do python task.py $RMQ_HOST $RMQ_PORT $new_file task &> $WORK_DIR/$new_file"_".out; done
     local extra_mils=5000
     sleep 5
-    q_size=`python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json task_queue`
+    sned_index=0
+    for (( sned_i=0; sned_i<=$NUMBER_OF_NODES; sned_i++ ))
+    do  
+        q_name=`curl -s -u guest:guest http://$RMQ_HOST:15672/api/queues/ | jq -r .[$sned_i].name`
+        if [ $q_name = "task_queue" ]; then
+            sned_index=$sned_i
+            break
+        fi
+    done
+    q_size=`curl -s -u guest:guest http://$RMQ_HOST:15672/api/queues/ | jq -r .[$sned_index].messages`
+#     q_size=`python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json task_queue`
 #     echo task_queue $q_size
     while [ $q_size -ge 1 ]
     do
 #         echo task_queue $q_size
-        extra_mils=$((extra_mils+100))
-#         sleep 0.1
-        q_size=`python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json task_queue`
+#         q_size=`python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json task_queue`
+        q_size=`curl -s -u guest:guest http://$RMQ_HOST:15672/api/queues/ | jq -r .[$sned_index].messages`
         count=$((count+1))
+        extra_mils=$((extra_mils+500))
+        sleep 0.5        
     done
     
     python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json consume
-    q_size=`python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json print_consume`
+    
+    for (( sned_i=0; sned_i<=$NUMBER_OF_NODES; sned_i++ ))
+    do  
+        q_name=`curl -s -u guest:guest http://$RMQ_HOST:15672/api/queues/ | jq -r .[$sned_i].name`
+        if [ $q_name = "task_queue_done" ]; then
+            sned_index=$sned_i
+            break
+        fi
+    done    
+#     q_size=`python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json print_consume`
+    q_size=`curl -s -u guest:guest http://$RMQ_HOST:15672/api/queues/ | jq -r .[$sned_index].messages`
 #     echo task_queue_done $q_size
     while [ $q_size -ge 1 ]
     do
 #         echo task_queue_done $q_size
-        extra_mils=$((extra_mils+100))
-#         sleep 0.1
+        extra_mils=$((extra_mils+500))
+        sleep 0.5
         python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json consume
-        q_size=`python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json print_consume`
+#         q_size=`python task.py $RMQ_HOST $RMQ_PORT 0_configuration_new.json print_consume`
+        q_size=`curl -s -u guest:guest http://$RMQ_HOST:15672/api/queues/ | jq -r .[$sned_index].messages`
     done
     END_EXECUTION=$(($(date +%s%N)/1000000))
     END_EXECUTION=$((END_EXECUTION-extra_mils))
@@ -296,7 +316,6 @@ function run_parameter_sweep_distributed_ssh() {
 function run_parameter_sweep_distributed_rabbit() {
     all_parameters="9, 11, 12, 13, 20, 28, 30, 35, 43, 44, 45, 50, 54, 55, 58, 59, 60, 61, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 82, 83, 84, 87, 88, 89, 90, 94, 95, 96, 97, 98, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 114, 124, 132, 133, 135, 139, 145, 146, 147, 148, 150, 151, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 180, 183, 184, 188, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444, 445, 500, 501, 502, 504, 505, 506, 508, 509,  510, 511, 512, 513, 514, 515, 516, 517, 520, 521, 522, 523, 524, 525, 526, 534, 535, 536, 542, 543, 544, 545, 546, 547, 548, 549, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 575, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 606, 607, 608, 609, 610, 611, 612, 613, 614, 615, 616, 617, 618, 619, 620, 621, 622, 623, 624, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634, 635, 636, 637, 638, 639, 640, 641, 642, 643, 644, 645"    
     local GLOBAL_COUNT=0
-    local nodes=`python getNumberOfConsumers.py $RMQ_HOST 15672 task_queue`
     #Set latitude
 #     for (( i_rabbit=$LAT_START; i_rabbit<=$MAX_LAT; i_rabbit=i_rabbit+$STEP ))
 #     do
@@ -315,26 +334,26 @@ function run_parameter_sweep_distributed_rabbit() {
 #                     local parameters=$parameters","$l
 #                     if [ "$count" -gt "200" ]; then
 #                         newConf $1 $i_rabbit $j $NEXT_DATE $parameters
-#                         python partitioning.py configuration_new.json $nodes
+#                         python partitioning.py configuration_new.json $NUMBER_OF_NODES
 #                         send_messages
 #                         local GLOBAL_COUNT=$((GLOBAL_COUNT+1))
 #                         local count=0
 #                     fi
 #                 done <physical_parameter_keys.txt
 #                 newConf $1 $i_rabbit $j $NEXT_DATE $parameters
-#                 python partitioning.py configuration_new.json $nodes
+#                 python partitioning.py configuration_new.json $NUMBER_OF_NODES
 #                 send_messages        
 #             done        
 #             newConf $1 $i_rabbit $j $MAX_DATE $parameters
-#             python partitioning.py configuration_new.json $nodes
+#             python partitioning.py configuration_new.json $NUMBER_OF_NODES
 #             send_messages
 #         done
 #         newConf $1 $i_rabbit $MAX_LON $MAX_DATE $parameters
-#         python partitioning.py configuration_new.json $nodes
+#         python partitioning.py configuration_new.json $NUMBER_OF_NODES
 #         send_messages
 #     done
     newConf $1 $MAX_LAT $MAX_LON $MAX_DATE "9, 11, 12, 13, 20, 28, 30, 35, 43, 44, 45, 50, 54, 55, 58, 59, 60, 61, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 82, 83, 84, 87, 88, 89, 90, 94, 95, 96, 97, 98, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 114, 124, 132, 133, 135, 139, 145, 146, 147, 148, 150, 151, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 180, 183, 184, 188, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 300, 301, 302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317, 318, 319, 320, 321, 322, 323, 324, 325, 326, 327, 328, 329, 330, 331, 332, 333, 334, 335, 336, 337, 338, 339, 340, 341, 342, 343, 344, 345, 346, 347, 348, 349, 350, 351, 352, 353, 354, 355, 356, 357, 358, 359, 360, 361, 362, 363, 364, 365, 366, 367, 368, 369, 370, 371, 372, 373, 374, 375, 376, 377, 378, 379, 380, 381, 382, 383, 384, 385, 386, 387, 388, 389, 390, 391, 392, 393, 394, 395, 396, 397, 398, 399, 400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 419, 420, 421, 422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 435, 436, 437, 438, 439, 440, 441, 442, 443, 444, 445, 500, 501, 502, 504, 505, 506, 508, 509,  510, 511, 512, 513, 514, 515, 516, 517, 520, 521, 522, 523, 524, 525, 526, 534, 535, 536, 542, 543, 544, 545, 546, 547, 548, 549, 551, 552, 553, 554, 555, 556, 557, 558, 559, 560, 561, 562, 563, 564, 565, 566, 567, 568, 569, 570, 571, 572, 573, 575, 576, 577, 578, 579, 580, 581, 582, 583, 584, 585, 586, 587, 588, 589, 590, 591, 592, 593, 594, 595, 596, 597, 598, 599, 600, 601, 602, 603, 604, 605, 606, 607, 608, 609, 610, 611, 612, 613, 614, 615, 616, 617, 618, 619, 620, 621, 622, 623, 624, 625, 626, 627, 628, 629, 630, 631, 632, 633, 634, 635, 636, 637, 638, 639, 640, 641, 642, 643, 644, 645"
-    python partitioning.py configuration_new.json $nodes
+    python partitioning.py configuration_new.json $NUMBER_OF_NODES
     send_messages
 }
 
@@ -374,6 +393,8 @@ fi
 RMQ_HOST=localhost 
 RMQ_PORT=5672
 DBG=true     
+
+NUMBER_OF_NODES=`python getNumberOfConsumers.py $RMQ_HOST 15672 task_queue`
 
 
 if [ -n "$CONF_FILE" ]; then
