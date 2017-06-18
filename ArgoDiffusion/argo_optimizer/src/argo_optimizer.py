2ce8efb4344b2d31f1183814959fb2811473f800
skoulouzis <S.Koulouzis@uva.nl>
2017-06-18 18:32:27 +0200

    bug

--- ArgoDiffusion/argo_optimizer/src/argo_optimizer.py
+++ ArgoDiffusion/argo_optimizer/src/argo_optimizer.py
@@ -74,7 +74,7 @@ if __name__ == "__main__":
                 start_time = timeit.default_timer()
 #                task = db.get_first_task()
 #                task = db.get_last_task()
-                test_time_range = {const.time_start_tag:"1999-01-01T00:00:19Z", const.time_end_tag:"2002-01-01T00:00:19Z"}
+                test_time_range = {const.time_start_tag:"1999-01-01T00:00:19Z", const.time_end_tag:"2004-01-01T00:00:19Z"}
                 tasks = db.get_tasks_in_time_range(test_time_range)
                 task = tasks[0]
                 
