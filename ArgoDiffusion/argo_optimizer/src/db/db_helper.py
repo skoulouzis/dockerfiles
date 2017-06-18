0ef0fdf72c0d18722975c39cb6e249181546caad
skoulouzis <S.Koulouzis@uva.nl>
2017-06-18 17:56:35 +0200

    bug

--- ArgoDiffusion/argo_optimizer/src/db/db_helper.py
+++ ArgoDiffusion/argo_optimizer/src/db/db_helper.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import pymongo
 from pymongo import Connection
@@ -36,16 +37,31 @@ class DBHelper:
                                              {"$or":[
                                              {self.const.executed_tag: {"$exists": False}},
                                              {self.const.executed_tag: {"$eq": False}}]}, 
-                                             sort=[(self.const.execution_rank_tag, pymongo.DESCENDING)])                                             
+                                             sort=[(self.const.execution_rank_tag, pymongo.DESCENDING)])  
+    
+    def get_tasks_in_time_range(self, time_range):
+        time_end = time_range[self.const.time_end_tag]
+        time_start = time_range[self.const.time_start_tag]
+        if (isinstance(time_range[self.const.time_end_tag], str) or isinstance(time_range[self.const.time_end_tag], unicode)):
+            time_end = datetime.strptime(time_range[self.const.time_end_tag], self.const.date_format)
+        if (isinstance(time_range[self.const.time_start_tag], str) or isinstance(time_range[self.const.time_start_tag], unicode)):
+            time_start = datetime.strptime(time_range[self.const.time_start_tag], self.const.date_format)
+                     
+        return self.task_collection.find({
+                                         self.const.time_tag + "." + 
+                                         self.const.time_end_tag:{"$eq":time_end},
+                                         self.const.time_tag + "." + 
+                                         self.const.time_start_tag:{"$eq":time_start}
+                                         })                                   
             
     def delete_task(self, task):
         self.task_collection.delete(task)
-        
+
     def mark_task_done(self, task):
         task[self.const.executed_tag] = True
         doc_id = task['_id']
         self.task_collection.update({'_id':doc_id}, task)
-        
-        
+
+
     def get_num_of_docs(self):
-        return self.task_collection.count()
+        return self.task_collection.count() 
